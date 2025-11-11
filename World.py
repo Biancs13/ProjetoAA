7375b0714efea9d8f20d4d0a984ef7e32ad1a89d
LEI-122674 <bspca1@iscte-iul.pt>
2025-11-11 13:59:50 +0000

    Added Files

---
+++ World.py
@@ -0,0 +1,43 @@
+class World:
+    def __init__(self, gridSize, goal):
+        self.map = {(x,y):None for x in range(gridSize) for y in range(gridSize)}
+        self.gridSize = gridSize
+        self.agents = [goal]
+        self.goal = goal
+        self.addAgent(goal)
+
+    def addAgent(self, agent):
+        if agent is not None and self.map.get(agent.getPosition()) is None:
+            self.agents.append(agent)
+            self.map[agent.getPosition()] = agent
+
+    def updateAgent(self,agent,i):
+        position = agent.getPosition()
+        self.map[position] = None
+        agent.randomMove(self.gridSize,self)
+        self.map[agent.getPosition()] = agent
+        if agent.getPosition() == self.goal.getPosition():
+            print(f"Agent {agent.getName()} reached the goal in {i} moves!")
+            return True
+        return False
+    
+    
+    def updateWorldRandom(self,i):
+        outcome = False
+        for a in self.agents:
+            if a is not self.goal:
+                outcome = outcome or self.updateAgent(a,i)
+        return outcome
+
+    def toString(self):
+
+        for y in range(self.gridSize):
+            line = " "
+            for x in range(self.gridSize):
+                if self.map[(x,y)] is None:
+                    line += ". "
+                else:
+                   line += self.map[(x,y)].getName() + " "
+            print(line)
+        print()
+    
---
+++ main.py
@@ -0,0 +1,24 @@
+from World import World
+from Agent import Agent
+import random
+
+def main():
+    wantSee = True
+    n = 40
+    g = Agent("G", (2,2))
+    world = World(5,g)
+    a1 = Agent("A", (0,0))
+    a2 = Agent("B", (1,1))
+    world.addAgent(a1)  
+    world.addAgent(a2)
+
+    if wantSee:
+        for i in range(n):
+            world.toString()
+            if world.updateWorldRandom(i):
+                return
+            
+
+
+if __name__ == "__main__":
+    main()
