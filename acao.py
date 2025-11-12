c4b6c489e22c3a78b14939a08d44600a10d67bb9
Biancs13 <bianca.sp.correia@hotmail.com>
2025-11-12 14:14:14 +0000

    Added Base Files

---
+++ acao.py
@@ -0,0 +1,21 @@
+from enum import Enum
+from posicao import Posicao
+
+
+def Acao(Enum):
+    DIREITA = (1,0)
+    ESQUERDA = (-1,0)
+    CIMA =  (0,-1)
+    BAIXO =  (0,1)
+
+    def getNovaPosicao(p):
+        x = p.getX()
+        y = p.getY()
+        if p == DIREITA:
+            return Posicao(x+1,y)
+        elif p == ESQUERDA:
+            return Posicao(x-1,y)
+        elif p == CIMA:
+            return Posicao(x,y-1)
+        return Posicao(x,y+1)
+
