4dc6599ef1cd0ae99e81d684e7eda3153a746d9c
Bianca Correia <bspca1@iscte-iul.pt>
2025-12-05 12:17:09 +0000

    UAi 37

--- controladorReforco.py
+++ controladorReforco.py
@@ -13,13 +13,22 @@ class ControladorReforco():
 
     def executar(self):
         q = {}
-        for _ in range(self.episodios):
+        for i in range(self.episodios):
             motor = cria(self.ficheiro_motor,self.problema,"reforco",self.tempo)
             motor.agentes[0].q = q
             motor.executa() # aqui treinamos
             q = motor.agentes[0].q
+            print(i,q)
 
         # TODO pôr aqui para guardar o melhor individuo
+
+
+def get_max_recompensa_q(q):
+    max_val = 0
+    for estado, dic_acoes in q.items():
+        if dic_acoes:
+            max_val = max(max_val, max(dic_acoes.values()))
+    return max_val
 
 
 
